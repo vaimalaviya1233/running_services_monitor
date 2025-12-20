270af30837e5bc6a58ff6c7e8892d1f191243265
Biplob Sutradhar <biplobsd11@gmail.com>
2025-12-20 14:21:41 +0600

    default

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -94,13 +94,13 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
 
-val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 3)
-android.applicationVariants.configureEach {
-    val variant = this
-    variant.outputs.forEach { output ->
-        val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
-        if (abiVersionCode != null) {
-            (output as ApkVariantOutputImpl).versionCodeOverride = variant.versionCode * 10 + abiVersionCode
-        }
-    }
-}
+// val abiCodes = mapOf("armeabi-v7a" to 1, "arm64-v8a" to 2, "x86_64" to 4)
+// android.applicationVariants.configureEach {
+//     val variant = this
+//     variant.outputs.forEach { output ->
+//         val abiVersionCode = abiCodes[output.filters.find { it.filterType == "ABI" }?.identifier]
+//         if (abiVersionCode != null) {
+//             (output as ApkVariantOutputImpl).versionCodeOverride = abiVersionCode * 1000 + variant.versionCode
+//         }
+//     }
+// }
